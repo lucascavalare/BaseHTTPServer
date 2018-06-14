83da64beca30184adeb0070cc95e5f0e5a18ce3c
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-14 20:22:43 -0300

    Delete queryparser2.py

--- CEPs/queryparser2.py
+++
@@ -1,9 +0,0 @@
-import urlparse
-
-# Separa os pares (nome,valor) contidos numa url contendo os
-# dados de um formulario devolvidos pelo pelo navegador (usando GET).
-# Limitacao: esta versao nao trata listas de selecao multipla.
-
-def parse(url):
-    query = urlparse.urlparse(url)[4]
-    print query
