8fa86a62fb523c4f82afcdd4f0e2ca3e7373b803
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-15 17:11:48 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -15,7 +15,7 @@ for file in os.listdir('.'):
         f = open(file)
         txt = f.read()
         lista = json.loads(txt)
-        print lista[CEP]
+        print lista[0]
         #return lista
         
 def criaMapa(lista):
