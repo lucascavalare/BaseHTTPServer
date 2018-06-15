f9107630d6785336512b0006329de4ab8b0d60dd
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-15 17:03:47 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -43,7 +43,7 @@ def consulta(cep):
         print 'CEP', cep, 'nao encontrado'
 
 print 'inicio'
-mapa = criaMapa('69301000')
+mapa = criaMapa('uf')
 print 'mapa criado tamanho:',len(mapa)
 '''
 consulta('77001004')
