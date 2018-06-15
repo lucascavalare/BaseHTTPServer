b4f3e6cdf781e004fb7eb75ffc274e081b1d403c
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-15 20:00:06 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -27,7 +27,7 @@ for file in os.listdir('.'):
                 dados = mapa[cep]
                 jsonData = json.dumps(dados)
                 if '49037563' in jsonData:
-                    print(dados,indent=8)
+                    print(dados, indent=8)
                 #print(jsonData)
                 #python 3: a sintaxe do print é diferente
                 #print 'CEP:',cep,'Estado:',jsonData[3],'Cidade:',jsonData[5],'Bairro:',jsonData[4]
