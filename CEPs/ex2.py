ffbf3c3ea334a69b1a00d8076cb38a6951e22bca
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-18 17:30:19 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -27,10 +27,10 @@ def consulta(cep):
                     dados = mapa[cep]
                     jsonData = json.dumps(dados, indent=8)
                     if '49500244' in jsonData:
-                        dados[cep] = jsonData
+                        cep = jsonData['CEP']
                     #print(jsonData)
-                return(dados)
-print(consulta(dados))
+                return(cep)
+print(consulta(cep))
        
                 #python 3: a sintaxe do print é diferente
                 #print 'CEP:',cep,'Estado:',jsonData[3],'Cidade:',jsonData[5],'Bairro:',jsonData[4]
