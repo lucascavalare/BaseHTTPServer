2d0fd53ccbbbe1175bea9756ef71fbcf9321731f
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-18 19:40:04 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -50,11 +50,13 @@ class ServidorExemplo(BaseHTTPServer.BaseHTTPRequestHandler):
     # tratamento de uma requisicao POST
     def do_POST(self):
         self.wfile.write("<HTML><body>Operação POST não permitida.<BR><BR></body></HTML>");
+
 # criação do servidor            
-#httpserver = BaseHTTPServer.HTTPServer(("",8080), ServidorExemplo)
+httpserver = BaseHTTPServer.HTTPServer(("",8080), ServidorExemplo)
       
-      rodar até ...
+#rodar até ...
 httpserver.serve_forever()
+
 '''      
 def criaMapa(lista):
     #for file in os.listdir('.'):
