f3ceab4a47365fa221e332baf1e687dc43c7f745
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-18 18:44:16 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -33,7 +33,7 @@ def consulta(cep):
                 #else:
                     #print 'CEP', cep, 'não encontrado'
 
-consulta('69301011')
+print(consulta('69301011'))
 
                     #jsonData = json.dumps(dados, indent=8)
                     #if '49500244' in jsonData:
