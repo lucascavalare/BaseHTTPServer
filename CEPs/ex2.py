5ba53d231faf6560f7580356e9795c2cec0d6b3a
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-19 18:09:27 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -64,7 +64,7 @@ class ServidorExemplo(BaseHTTPServer.BaseHTTPRequestHandler):
                     result = carrega('cep')
                 else:
                     result = { 'erro': 'parametro "cep" ausente' }
-        self.wfile.write(carrega(self.path))
+        self.wfile.write(result)
 
     # tratamento de uma requisicao POST
     def do_POST(self):
