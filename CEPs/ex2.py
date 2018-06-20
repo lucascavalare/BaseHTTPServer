59866034cd83b063a948eff31fb69d7cc491efe2
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-19 21:09:57 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -25,7 +25,7 @@ for file in os.listdir('.'):
         f = open(file)
         txt = f.read()
         lista = json.loads(txt)
-            #print(lista)
+        print lista
         
             global mapa
             mapa = {}
