fad9fe9c225d5673050dc03e8316e49218eef1d5
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-20 17:28:38 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -6,20 +6,22 @@ import json
 import os
 import fnmatch
 
-for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, '*.json'):
-        f = open(file)
-        txt = f.read()
-        lista = json.loads(txt)
+def consulta(cep)
+
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, '*.json'):
+            f = open(file)
+            txt = f.read()
+            lista = json.loads(txt)
         
-        mapa = {}
-        for elemento in lista:
-            cep = elemento['CEP']
-            mapa[cep] = elemento
-            if cep in mapa.keys():
-                dados = mapa[cep]
-                jsonData = json.dumps(dados, indent=8)
-                if '72800020' in jsonData:
-                    print(jsonData)
+            mapa = {}
+            for elemento in lista:
+                cep = elemento['CEP']
+                mapa[cep] = elemento
+                if cep in mapa.keys():
+                    dados = mapa[cep]
+                    jsonData = json.dumps(dados, indent=8)
+                    if '72800020' in jsonData:
+                        print(jsonData)
 
 
