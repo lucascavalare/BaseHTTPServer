d2df4c105c04e185efe3f48b0e0fb0c93630bf09
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-19 21:22:26 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -25,7 +25,7 @@ def carrega(cep):
             f = open(file)
             txt = f.read()
             lista = json.loads(txt)
-        return lista
+        #return lista
         
             global mapa
             mapa = {}
