3f284be627f12d9bd9c2c750d6d4efb2b996fbf1
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-20 16:57:43 -0300

    Update ex2.py

--- CEPs/ex2.py
+++ CEPs/ex2.py
@@ -1,1 +1,27 @@
+# -*- coding: cp1252 -*-
+
+
+
+import json
+
+
+def criaMapa(uf):
+    
+    #'abre' o arquivo para leitura
+    f = open('ceps_' + * + '.json', 'r')
+
+    # efetua a leitura do arquivo (dados lidos armazenados em txt)
+    txt = f.read()
+
+    # parsing do json (resultado em lista)
+    lista = json.loads(txt)
+
+    # criação do mapa
+    mapa = {}
+    for elemento in lista:
+        cep = elemento['CEP']
+        mapa[cep] = elemento    
+    return mapa
+
+consulta('69301000')
 
