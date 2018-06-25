d132537002a9e4f0826ef92f3e9d4f18f0d4fac4
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-25 19:37:36 -0300

    Update and rename ex2.py to ex3.py

---
+++ CEPs/ex3.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1252 -*-
+'''
+    Exemplo básico de servidor web
+    -- trata apenas o método GET
+    -- responde com um texto html fixo (htmlpage)
+'''
+
+import BaseHTTPServer, json, os, fnmatch
+import queryparser
+import Diretorio
+
+Diretorio.consultaEstados('SC')
+    print consultaEstados()
