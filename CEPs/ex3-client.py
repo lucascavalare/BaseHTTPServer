3cd7294ad3242cccdb1488ddda6dad5dedfcb5e1
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-25 20:21:50 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -12,27 +12,7 @@
 import httplib
 import sys
 
-#get http server ip
-http_server = sys.argv[1]
-#create a connection
-conn = httplib.HTTPConnection(http_server)
-'''
-while 1:
-  cmd = raw_input('input command (ex. GET index.html): ')
-  cmd = cmd.split()
-
-  if cmd[0] == 'exit': #tipe exit to end it
-    break
-  
-  #request command to server
-  conn.request(cmd[0], cmd[1])
-'''
-  #get response from server
-rsp = conn.getresponse()
-  
-  #print server response and data
-print(rsp.status, rsp.reason)
-data_received = rsp.read()
-print(data_received)
-  
-conn.close()   
+conn = httplib.HTTPSConnection('35.237.56.163', 8080)
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print r1.status, r1.reason
