dc3670d603de1f3fd33b7736d15b7628e1af2ea9
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-25 20:04:13 -0300

    Update and rename ex3.py to ex3-client.py

---
+++ CEPs/ex3-client.py
@@ -0,0 +1,38 @@
+# -*- coding: cp1252 -*-
+'''
+    Exemplo básico de servidor web
+    -- trata apenas o método GET
+    -- responde com um texto html fixo (htmlpage)
+'''
+
+#import BaseHTTPServer, json, os, fnmatch
+#import queryparser
+import Diretorio
+
+import httplib
+import sys
+
+#get http server ip
+http_server = sys.argv[1]
+#create a connection
+conn = httplib.HTTPConnection(http_server)
+
+while 1:
+  cmd = raw_input('input command (ex. GET index.html): ')
+  cmd = cmd.split()
+
+  if cmd[0] == 'exit': #tipe exit to end it
+    break
+  
+  #request command to server
+  conn.request(cmd[0], cmd[1])
+
+  #get response from server
+  rsp = conn.getresponse()
+  
+  #print server response and data
+  print(rsp.status, rsp.reason)
+  data_received = rsp.read()
+  print(data_received)
+  
+conn.close()   
