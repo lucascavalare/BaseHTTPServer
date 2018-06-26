d7c9601444d85615c5368fd9628c95537ab9ebcc
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-26 19:32:40 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -9,10 +9,14 @@
 #import queryparser
 #import Diretorio
 
-import httplib
-import sys
-
+#import httplib
+#import sys
+import httplib2
+'''
 conn = httplib.HTTPConnection('35.196.162.70', 8080)
 conn.request("GET", "/?op=estados")
 r1 = conn.getresponse()
 print r1.status, r1.reason
+'''
+h = httplib2.Http(".cache")
+resp, content = h.request("http://35.196.162.70:8080", "GET")
