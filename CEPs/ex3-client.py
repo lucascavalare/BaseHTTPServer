b1accf29153fcc1469ec8139b2707c157b5fa1fd
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-26 19:37:55 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -21,4 +21,4 @@ print r1.status, r1.reason
 '''
 h = httplib2.Http(".cache")
 resp, content = h.request("http://35.196.162.70:8080", "GET")
-print json.dumps(resp)
+print json.dumps(resp, indent=5)
