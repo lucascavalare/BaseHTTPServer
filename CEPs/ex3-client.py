ba73406c916b1bc5b0fef4c66a5b01004c3cada5
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-28 19:47:29 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -14,9 +14,17 @@
 #import sys
 #import httplib2
 #import json, os
-import socket 
+#import socket 
+import requests 
+
+url = "http://35.229.35.219:8080/?op=estados"
+headers = {'cache-control': "no-cache"}
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
 
 
+'''
 HOST = '35.229.35.219'  # The remote host
 PORT = 8080             # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,4 +35,4 @@ conn, addr = s.accept()
 print 'Connected by', addr
 
 #def constultaCEP('69301015', 'RR')
-    
+'''    
