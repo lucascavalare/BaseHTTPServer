812841868f310591ffa7a62aea54fc4c0b2a4a26
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-28 18:55:32 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -17,7 +17,7 @@
 import socket 
 
 
-HOST = '35.229.35.219'  # The remote host
+HOST = '0.0.0.0'  # The remote host
 PORT = 8080             # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
