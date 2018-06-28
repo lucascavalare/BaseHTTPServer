5ade83e9b02d2cce1610b1a42121e436e0cd183b
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-28 18:45:29 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -9,43 +9,21 @@
 #import queryparser
 #import Diretorio
 
-import urlparse
+#import urlparse
 #import httplib
 #import sys
-import httplib2
-import json, os
+#import httplib2
+#import json, os
+import socket 
 
 
+HOST = '35.229.35.219'  # The remote host
+PORT = 8080             # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.binf((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
 
-url = 'http://35.229.35.219:8080/?'
-self.request.get('op')
-parsed = urlparse.urlparse(url) 
-print urlparse.parse_qs(parsed.query)['op'] 
-
-
-'''
-conn = httplib.HTTPConnection('35.196.162.70', 8080)
-conn.request("GET", "/?op=estados")
-r1 = conn.getresponse()
-print r1.status, r1.reason
-
-h = httplib2.Http(".cache")
-resp, content = h.request("http://35.196.162.70:8080/?op=registro&estado=MG&url=http://mg.gov.br:8080", "PUT", headers={'content-type':'text/json'})
-print json.dumps(resp, indent=5)
-'''
-'''
-def test1(cep, url):
-    #d = {"SP":"souza:8080", "RJ":"rodrigues:8080","RN": "silva:8080", "RO":"oliveira:8080", "MG":"pereira:8080"}
-    d = {"MG":"mg.gov.br:8080"}
-    r = {}
-    #for s in d.keys():
-    #    r[registraServidor(s,d[s])] = d[s]
-    #print r
-    #print consultaEstados()
-    for s in d.keys():
-        print consultaServidor(s)
-    #print consultaServidor('kk')
-    #print consultaServidor('pp')
-    #for k in r.keys():
-    #    print removeServidor(r[k],k)
-'''
+def constultaCEP('69301015', 'RR')
+    
