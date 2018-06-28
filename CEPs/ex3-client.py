f6a182bf8aac4a150044ce4a5e246e1cd239f9c2
Lucas Cavalare <lucascavalare@gmail.com>
2018-06-28 15:05:46 -0300

    Update ex3-client.py

--- CEPs/ex3-client.py
+++ CEPs/ex3-client.py
@@ -18,7 +18,23 @@ conn = httplib.HTTPConnection('35.196.162.70', 8080)
 conn.request("GET", "/?op=estados")
 r1 = conn.getresponse()
 print r1.status, r1.reason
-'''
+
 h = httplib2.Http(".cache")
 resp, content = h.request("http://35.196.162.70:8080/?op=registro&estado=MG&url=http://mg.gov.br:8080", "PUT", headers={'content-type':'text/json'})
 print json.dumps(resp, indent=5)
+'''
+
+def test1(cep, url):
+    #d = {"SP":"souza:8080", "RJ":"rodrigues:8080","RN": "silva:8080", "RO":"oliveira:8080", "MG":"pereira:8080"}
+    d = {"MG":"mg.gov.br:8080"}
+    r = {}
+    for s in d.keys():
+        r[registraServidor(s,d[s])] = d[s]
+    print r
+    print consultaEstados()
+    for s in d.keys():
+        print consultaServidor(s)
+    print consultaServidor('kk')
+    print consultaServidor('pp')
+    for k in r.keys():
+print removeServidor(r[k],k)
